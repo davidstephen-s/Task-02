498d9a25fcaf77253d2abd6d5748843e29f4f21e
davidstephen-s <82367737+davidstephen-s@users.noreply.github.com>
2021-04-15 23:15:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// CREATE a REQUEST INSTANCE
+var req = new XMLHttpRequest();
+//initate a connection or create a connection
+req.open('GET','https://restcountries.eu/rest/v2/all',true);
+//sending the request
+req.send();
+//load the function
+//this function will be triggered only when  the data has been received  successfully;
+req.onload=function(){
+var data=JSON.parse(this.response);
+for(let i in data){
+console.log(data[i].name);
+}
+}
